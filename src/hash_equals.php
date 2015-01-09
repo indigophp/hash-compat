f7b57479e8b62f7c73d4e190771512f932868c55
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-01-09 03:23:12 +0100

    Coding style

--- src/hash_equals.php
+++ src/hash_equals.php
@@ -35,32 +35,28 @@ if (!function_exists('hash_equals')) {
         }
 
         // Check $known_string type
-        if (!is_string($known_string))
-        {
+        if (!is_string($known_string)) {
             trigger_error(sprintf('hash_equals(): Expected known_string to be a string, %s given', strtolower(gettype($known_string))), E_USER_WARNING);
 
             return false;
         }
 
         // Check $user_string type
-        if (!is_string($user_string))
-        {
+        if (!is_string($user_string)) {
             trigger_error(sprintf('hash_equals(): Expected user_string to be a string, %s given', strtolower(gettype($user_string))), E_USER_WARNING);
 
             return false;
         }
 
         // Compare string lengths
-        if (($length = strlen($known_string)) !== strlen($user_string))
-        {
+        if (($length = strlen($known_string)) !== strlen($user_string)) {
             return false;
         }
 
         $diff = 0;
 
         // Calculate differences
-        for ($i = 0; $i < $length; $i++)
-        {
+        for ($i = 0; $i < $length; $i++) {
             $diff |= ord($known_string[$i]) ^ ord($user_string[$i]);
         }
 
