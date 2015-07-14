2eac957326cadb7b6de9a6c924c37c7a4eaac771
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-07-14 23:53:39 +0200

    Update package files

--- src/hash_pbkdf2.php
+++ src/hash_pbkdf2.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the hash package.
- *
- * (c) Indigo Development Team
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 if (!function_exists('hash_pbkdf2')) {
     defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
 
